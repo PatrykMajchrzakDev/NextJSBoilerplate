c7664190f8f11dc2b811534def87e0968d3017a0
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-02-04 19:14:52 +0100

    Added initial app auth directory layout and created register page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+// =================================================
+// ======== LAYOUT FOR AUTH ROUTE CHILDREN =========
+// =================================================
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="w-full h-auto">
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="w-full max-w-[450px] mx-auto h-auto ">{children}</div>
+      </div>
+    </div>
+  );
+}
