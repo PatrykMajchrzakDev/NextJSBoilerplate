a73a7d1d1a89192442c457476422c18b3eca117c
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-02-06 18:46:41 +0100

    Changed look of register form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({
   return (
     <div className="w-full h-auto">
       <div className="w-full h-full flex items-center justify-center">
-        <div className="w-full max-w-[450px] mx-auto h-auto ">{children}</div>
+        {children}
       </div>
     </div>
   );
