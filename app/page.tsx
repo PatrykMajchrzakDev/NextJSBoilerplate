56f6fa30347ad5b6a3c6767cd0ccda6e06e01019
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-02-08 00:00:44 +0100

    Just testing the main route

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageWrapper from "@/context/page-wrapper";
+
+const page = () => {
+  return (
+    <PageWrapper>
+      <div className=" w-full mt-[1rem] p-3">/ page</div>
+    </PageWrapper>
+  );
+};
+
+export default page;
