23aa41b9013fffe04f06ec26a03c03094e4ee716
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-02-11 18:53:52 +0100

    Fixed typo

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -234,7 +234,7 @@ export default function SignUp() {
                 <div className="relative hidden bg-muted md:block">
                   <img
                     src="/auth/signup-side-img.webp"
-                    alt="Side photo showing plane and travelling accessories"
+                    alt="Side photo showing plane and traveling accessories"
                     className="absolute inset-0 h-full object-cover dark:brightness-[0.2] dark:grayscale"
                   />
                 </div>
