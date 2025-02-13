e019c50accc83fb3d50c6c638669aa148c16c150
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-02-13 19:26:18 +0100

    Changed navbar dark theme color

--- components/customUI/nav/Navbar.tsx
+++ components/customUI/nav/Navbar.tsx
@@ -37,7 +37,7 @@ export default function NavBar() {
       initial={{ y: -20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="fixed top-0 left-0 right-0 z-50 border-b backdrop-blur supports-[backdrop-filter]:bg-background/60 bg-white/80 dark:bg-black/80"
+      className="fixed top-0 left-0 right-0 z-50 border-b backdrop-blur supports-[backdrop-filter]:bg-background/60 bg-white/80 "
     >
       <div className="flex items-center justify-between p-4 lg:py-2 lg:px-4 max-w-7xl mx-auto">
         {/* This is mobile menu - Mobile */}
