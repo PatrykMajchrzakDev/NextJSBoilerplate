2c666b231a5b04d2e7bc07096b5353dc281d208f
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2025-02-16 19:43:17 +0100

    Updated app layout to include AuthProvider wrapper

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,18 @@
 // Layout for main route children
 
+import { AuthProvider } from "@/context/auth-provider";
+import PageWrapper from "@/context/page-wrapper";
+
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <AuthProvider>
+      <PageWrapper>
+        <div className="w-full h-full flex items-center justify-center">
+          {children}
+        </div>
+      </PageWrapper>
+    </AuthProvider>
+  );
 };
 
 export default MainLayout;
